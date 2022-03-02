94aaa08b9fbccc4825278b6adc61fbe761283f62
김준호 <80739316+rlawnsgh714@users.noreply.github.com>
2022-03-02 16:05:26 +0900

    Java-기초-문법-1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
+//Java 기초 문법 - 1
+
 package com.company;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //====================출력====================
+//
+//        //단축키 'sout' 입력시 위와 같이 나옴
+//        System.out.println("Hello, World!");
+//
+
+        //====================입력====================
+
+        //입력은 Scanner '이름' = new Scanner(System.in); 으로 한다.
+//        Scanner scanner = new Scanner(System.in);
+//        //Scanner 을 한 다음 정수형으로 변환 할 때 '이름'.nextInt();를 사용한다.
+//        int age = scanner.nextInt();
+//        //Scanner 을 한 다음 개행문자 제외 다 가져온다
+//        String s = scanner.nextLine();
+//        System.out.println(age);
+//
+        //====================형 변환====================
+//        byte var_int = 100;
+//        double var_double = 4.14;
+//        //다른 형을 가진 두 변수가 형 변환을 할 때 정하는 형으로 전환된다.
+          //형 변환시 큰 타입으로 자동 변환된다
+          //명시적으로 형 변환해 주지 않으면 오류가 발생한다.
+//        double result = var_int + var_double;
+//        System.out.println(result);
+
+
     }
 }
